9265df71bcabd9224b8c2bfa8959f5c144ebe697
Technifutur <barudy.diego@gmail.com>
2017-11-30 17:27:46 +0100

    Commit IA search

--- Assets/Ennemy.cs
+++ Assets/Ennemy.cs
@@ -16,18 +16,32 @@ public class Ennemy : MonoBehaviour
     #region System
     private void OnDrawGizmos()
     {
-       Gizmos.color = Color.red;
-       Vector3 vo = new Vector3(m_transform.position.x+1.5f, m_transform.transform.position.y, m_transform.transform.position.z);
-       Gizmos.DrawSphere(vo, 1f);
+      // Gizmos.color = Color.red;
+       
+
+       //Gizmos.DrawSphere(vo, 1f);
+       
 
     }
     void Start()
     {
+       
         m_rigidbody = GetComponent<Rigidbody>();
         m_transform = transform;
         m_col = GetComponent<Collider>();
         m_agent = GetComponent<NavMeshAgent>();
-        
+        Vector3 vo = new Vector3(m_transform.position.x, m_transform.transform.position.y, m_transform.transform.position.z);
+        coneRay[0] = new Ray(vo, new Vector3(10f, 0f, -2f));
+        coneRay[1] = new Ray(vo, new Vector3(10f, 0f, -1f));
+        coneRay[2] = new Ray(vo, new Vector3(10f, 0f, 0f));
+        coneRay[3] = new Ray(vo, new Vector3(10f, 0f, 1f));
+        coneRay[4] = new Ray(vo, new Vector3(10f, 0f, 2f));
+        coneRay[5] = new Ray(vo, new Vector3(10f, 1f, -1f));
+        coneRay[6] = new Ray(vo, new Vector3(10f, 1f, 1f));
+        coneRay[7] = new Ray(vo, new Vector3(10f, -1f, -1f));
+        coneRay[8] = new Ray(vo, new Vector3(10f, -1f, 1f));
+        coneRay[9] = new Ray(vo, new Vector3(10f, 2f, 0f));
+        coneRay[10] = new Ray(vo, new Vector3(10f, -2f, 0f));
     }
 
     void Update()
@@ -78,10 +92,12 @@ public class Ennemy : MonoBehaviour
 
         
         Vector3 vo = new Vector3(m_transform.position.x, m_transform.transform.position.y, m_transform.transform.position.z);
+
         Vector3 vd = new Vector3(0.5f, 0f, 0f);
         Vector3 vdsphere = new Vector3(0.09f, 0f, 0f);
         Ray aim = new Ray(vo, vd);
         Ray aimSphere = new Ray(vo, vdsphere);
+        ConeRayDetect(coneRay);
         if (_debug)
         {
             //Debug.DrawRay(vo, vd, Color.green);
@@ -115,7 +131,26 @@ public class Ennemy : MonoBehaviour
 
 
     }
+    private bool ConeRayDetect(Ray[] _coneRay,bool _debug=false) {
 
+       
+         for(int i= 0;i<_coneRay.Length;i++) {
+            if (_debug) { 
+            Debug.DrawRay( _coneRay[i].origin,_coneRay[i].direction*10f, Color.blue);
+            }
+
+            if (Physics.Raycast(_coneRay[i].origin, _coneRay[i].direction * 10f, out hit, 10f)) {
+                if (hit.transform.name=="Player")
+                {
+                    return true;
+                }
+
+            };
+            
+        }
+        return false;
+        //return hit;
+    }
     private void PlayerSpotted()
     {
         PlayerFollow();
@@ -179,5 +214,6 @@ public class Ennemy : MonoBehaviour
     private RaycastHit hit = new RaycastHit();
     private RaycastHit sphereHit = new RaycastHit();
     private NavMeshAgent m_agent;
+    private Ray[] coneRay= new Ray[11];
     #endregion
 }
